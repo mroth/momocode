60f5b9d9588f69bd9db2d88177a356be0c4ba892
Matthew Rothenberg <mrothenberg@gmail.com>
2018-03-05 16:40:12 -0500

    add chunked test

--- test/EmojiHashTest.js
+++ test/EmojiHashTest.js
@@ -2,26 +2,30 @@ var EmojiHash = artifacts.require("./EmojiHash.sol");
 
 const ex1_address = "0x583031D1113aD414F02576BD6afaBfb302140225";
 const ex1_encoded = "🎉🍜🍝👊🌺🍦👍🌽👩🍎🎵🐴🎩💀🐶🐪🌅🌽🌅🍎";
+const ex1_chunks = [
+"0xf09f8e89", "0xf09f8d9c", "0xf09f8d9d", "0xf09f918a", "0xf09f8cba",
+"0xf09f8da6", "0xf09f918d", "0xf09f8cbd", "0xf09f91a9", "0xf09f8d8e",
+"0xf09f8eb5", "0xf09f90b4", "0xf09f8ea9", "0xf09f9280", "0xf09f90b6",
+"0xf09f90aa", "0xf09f8c85", "0xf09f8cbd", "0xf09f8c85", "0xf09f8d8e"];
 
 contract("EmojiHash", async (accounts) => {
 
-    it("should encode an address with expected output", async () => {
+    it("should encode an address to string with expected output", async () => {
         let instance = await EmojiHash.deployed();
         let actual = await instance.encodeToStringDeprecated.call(ex1_address);
         assert.equal(actual, ex1_encoded);
     });
 
-    it("should encode an address with expected output FASTER", async () => {
+    it("should encode an address to string with expected output FASTER", async () => {
         let instance = await EmojiHash.deployed();
         let actual = await instance.encodeToString.call(ex1_address);
         assert.equal(actual, ex1_encoded);
     });
 
-    it("should encode BLAH BLAH bytes", async () => {
+    it("should encode to chunks for contract calls", async () => {
         let instance = await EmojiHash.deployed();
         let actual = await instance.encode.call(ex1_address);
-        console.log(actual);
-        // assert.equal(actual, expected);
+        assert.deepEqual(actual, ex1_chunks);
     });
 
 });
